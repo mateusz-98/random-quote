82196af2fd0ca0130f7140949855175928aa7f39
Matthew <makelo12345@gmail.com>
2024-04-06 13:25:07 +0200

    change api link and add copy function

--- script.js
+++ script.js
@@ -4,20 +4,64 @@ document.addEventListener("DOMContentLoaded", () => {
     const quote = document.querySelector(".quote-random");
     const newQuoteBtn = document.querySelector(".quote-new");
     const copyQuoteBtn = document.querySelector(".quote-copy");
-    const apiLink = "https://api.quotable.io/random";
+    const quoteModal = document.querySelector(".modal");
 
-    const randomQuote = async () => {
+    const getData = async () => {
+        const apiLink = "https://api.api-ninjas.com/v1/quotes";
+        const apiKey = "2jbFSsCIhlX/+HrbNxa2oA==4Bn7JdsTz2An3vP4";
+        const apiOptions = {
+            method: "GET",
+            headers: {
+                "X-Api-Key": apiKey,
+                "Content-type": "application/json",
+            },
+        }
+
         try {
             
-            const response = await fetch("https://goquotes-api.herokuapp.com/api/v1/random?count=1", {mode: "no-cors"});
+            const response = await fetch(apiLink, apiOptions);
+            
+            if(!response.ok) throw new Error(response.statusText);
+
             const jsondata = await response.json();
+
+            randomQuote(jsondata);
+
             console.log(jsondata);
 
-        } catch(error) {
-            console.log("Something gone wrong: ", error);
+        } catch(err) {
+            
+            author.textContent = "-";
+            tags.textContent = "-";
+            quote.textContent = "Something went wrong. Try later or refresh the page.";
+
+            console.log("Something went wrong: ", err);
+        }
+
+    }
+
+    const randomQuote = (data) => {
+        author.textContent = data[0].author;
+        tags.innerHTML = `<p class="quote-tag">${data[0].category}</p>`;
+        quote.textContent = `“${data[0].quote}”`;
+    }
+
+    const copyQuote = async () => {
+        try {
+            await navigator.clipboard.writeText(quote.textContent);
+            
+            quoteModal.classList.add("show");
+
+            setTimeout(() => {
+                quoteModal.classList.remove("show");
+            }, 4000);
+
+
+        } catch(err) {
+            console.log("You can't copy quoute: ", err);
         }
     }
 
-
-    newQuoteBtn.addEventListener("click", randomQuote);
+    newQuoteBtn.addEventListener("click", getData);
+    copyQuoteBtn.addEventListener("click", copyQuote);
 });
